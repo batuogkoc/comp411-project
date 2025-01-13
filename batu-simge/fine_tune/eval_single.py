190c2b90488dbb4adebb2c9686c7cc9b9c3584f8
batuogkoc <bgunduz21@ku.edu.tr>
2025-01-13 16:17:42 +0300

    moved folders around

--- batu-simge/fine_tune/eval_single.py
+++ batu-simge/fine_tune/eval_single.py
@@ -11,6 +11,7 @@ from datasets import load_dataset
 checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
 model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
 
+
 def predict_masks(predictor: SAM2ImagePredictor, image):
     with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
         predictor.set_image(image)
@@ -23,27 +24,28 @@ def predict_masks(predictor: SAM2ImagePredictor, image):
         # masks, _, _ = predictor.predict(point_coords=point_coords, point_labels=point_labels)
         return masks
 
+
 def _predict_masks_test():
     raw_dataset = load_dataset("kowndinya23/Kvasir-SEG")
     train_set = raw_dataset["train"]
 
     predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
 
-    sample_idxs = [0,1]
-    fig, axs = plt.subplots(len(sample_idxs),2+3)
+    sample_idxs = [0, 1]
+    fig, axs = plt.subplots(len(sample_idxs), 2 + 3)
     for i in sample_idxs:
         image = train_set[i]["image"]
         mask_gt = train_set[i]["annotation"]
         # name, image, mask = train_set[i].items()
-        axs[i,0].imshow(image)
-        axs[i,1].imshow(mask_gt)
+        axs[i, 0].imshow(image)
+        axs[i, 1].imshow(mask_gt)
         masks = predict_masks(predictor, image)
         print(masks.shape)
         for j, mask in enumerate(masks):
-            axs[i,2+j].imshow(masks[j])
-
+            axs[i, 2 + j].imshow(masks[j])
 
     fig.savefig("single.png")
+
 
 def _generate_masks():
     raw_dataset = load_dataset("kowndinya23/Kvasir-SEG")
@@ -53,23 +55,22 @@ def _generate_masks():
 
     sample_idxs = range(10)
     plt.axis("off")
-    fig, axs = plt.subplots(len(sample_idxs),2+3)
+    fig, axs = plt.subplots(len(sample_idxs), 2 + 3)
 
     for i in sample_idxs:
         image = np.array(train_set[i]["image"].convert("RGB"))
         mask_gt = train_set[i]["annotation"]
-        axs[i,0].imshow(image)
-        axs[i,1].imshow(mask_gt)
+        axs[i, 0].imshow(image)
+        axs[i, 1].imshow(mask_gt)
 
         with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
             masks = mask_generator.generate(image)
             for j, mask in enumerate(masks[:3]):
                 segmentation = mask["segmentation"]
-                axs[i,2+j].imshow(segmentation)
+                axs[i, 2 + j].imshow(segmentation)
 
     fig.savefig("eval_single.png")
 
+
 if __name__ == "__main__":
     _generate_masks()
-
-
--- valar_start_srun.sh
+++ valar_start_srun.sh
@@ -1,1 +1,1 @@
-srun -N 1 -n 4 --mem=64G --qos=users --partition=avg -t 4:00:00 --gres=gpu:1 --pty bash
+srun -N 1 -n 4 --mem=64G --qos=users --partition=ai -t 8:00:00 --gres=gpu:1 --pty bash
