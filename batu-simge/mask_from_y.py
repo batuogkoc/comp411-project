Fixed gradient problems in training. Cleaned up some code

batu-simge/mask_from_y.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 def extract_points_from_mask(mask, num_points, normalize=False):
     if not isinstance(mask, np.ndarray):
         if isinstance(mask, torch.Tensor):
-            mask = mask.cpu().numpy()
+            mask = mask.detach().cpu().numpy()
         else:
             mask = np.array(mask)
     if mask.max() > 1:
@@ -53,41 +53,23 @@ def visualize_points_on_mask(mask):
     plt.savefig("points.png")
 
 
-def generate_masks_with_points(num_points=3):
-    checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
-    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
-    predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
-
-    # raw_dataset = load_dataset("kowndinya23/Kvasir-SEG")
-    # train_set = raw_dataset["train"]
-    dataset = KvasirSEGDataset("train", transform=A.Compose([A.Resize(1024, 1024)]))
-    loader = DataLoader(dataset, 5, shuffle=False)
-    images, masks = next(iter(loader))
-    points_batch = extract_points_from_mask_batched(masks, num_points=num_points)
-    predictor.set_image_batch([*images])
-    masks_pred, _, _ = predictor.predict_batch(
-        point_coords_batch=points_batch, point_labels_batch=np.ones_like(points_batch)
-    )
-    sample_idxs = [0, 1, 2, 3]
-
+def generate_masks_with_points(predictor, dataset, num_points=3, num_samples=5):
     num_predicted_masks = 3
     fig, axs = plt.subplots(
-        len(sample_idxs), 2 + num_predicted_masks, figsize=(15, 5 * len(sample_idxs))
+        num_samples, 2 + num_predicted_masks, figsize=(15, 5 * num_samples)
     )
 
-    for row, (idx, points) in enumerate(zip(sample_idxs, points_batch)):
-        image, mask_gt = dataset[idx]
-        # image = np.array(image.convert("RGB"))
+    for row, (image, mask_gt) in enumerate([dataset[i] for i in range(num_samples)]):
+        mask_gt = (mask_gt > 0).astype(np.float32)
 
-        # mask_gt = train_set[idx]["annotation"].convert("L")
-        # points = extract_points_from_mask(mask_gt, num_points)
-        # print(points)
+        points = extract_points_from_mask(mask_gt, num_points)
         with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
             predictor.set_image(image)
             predicted_masks, _, _ = predictor.predict(
                 point_coords=points,
                 point_labels=np.ones(len(points)),
             )
+            predicted_masks = predicted_masks.detach().cpu().numpy()
         axs[row, 0].imshow(image)
         axs[row, 0].set_title("Original Image")
         axs[row, 0].scatter(
@@ -107,8 +89,7 @@ def generate_masks_with_points(num_points=3):
             axs[row, 2 + col].set_title(f"Predicted Mask {col + 1}")
             axs[row, 2 + col].axis("off")
 
-    plt.savefig("batu-simge/predicted_masks.png")
-    plt.show()
+    return fig
 
 
 def _visualize_test():
@@ -116,7 +97,17 @@ def _visualize_test():
     visualize_points_on_mask(mask_gt)
 
 
+def _multi_visualize_test():
+    checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
+    predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
+
+    dataset = KvasirSEGDataset("train", transform=A.Compose([A.Resize(1024, 1024)]))
+    fig = generate_masks_with_points(predictor, dataset, 10)
+    fig.savefig("batu-simge/predicted_masks.png")
+
+
 # generate_masks_with_points(num_points=3)
 if __name__ == "__main__":
     # _visualize_test()
-    generate_masks_with_points(3)
+    _multi_visualize_test()

batu-simge/train.py
@@ -24,7 +24,7 @@ def train_experiment():
 
     # Use wandb-core, temporary for wandb's new backend
     wandb.init(
-        project="linear-lossless-fir",
+        project="sam2-replication",
         name=EXPERIMENT_NAME,
         config={
             "rng_seed": 42,
@@ -42,9 +42,9 @@ def train_experiment():
                 "train_mask_decoder": True,
                 "train_prompt_encoder": True,
                 "train_image_encoder": False,
-                "num_points": 3,
+                "num_points": 10,
             },
-            "learning_rate": 3e-4,
+            "learning_rate": 1e-6,
         },
     )
     config = wandb.config
@@ -95,6 +95,7 @@ def train_experiment():
     predictor.model.sam_prompt_encoder.train(config["model"]["train_prompt_encoder"])
     predictor.model.sam_mask_decoder.train(config["model"]["train_mask_decoder"])
     loss_fn = torch.nn.BCEWithLogitsLoss()
+    # loss_fn = torch.nn.BCELoss()
     optimizer = optim.Adam(predictor.model.parameters(), lr=config["learning_rate"])
 
     state, metrics = train(

batu-simge/train_loop.py
@@ -18,7 +18,8 @@ from train_helpers import (
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 from torch import nn
 from torch import optim
-from mask_from_y import extract_points_from_mask
+from mask_from_y import extract_points_from_mask, generate_masks_with_points
+
 
 # def IoU(mask_pred, mask):
 #     intersection = torch.logical_and
@@ -40,126 +41,120 @@ def train(
     metrics: dict[str, torchmetrics.Metric] = {},
     num_points: int = 3,
 ):
-    with torch.no_grad():
-        if experiment_name is None:
-            experiment_name = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-        start_epoch = 0
+    if experiment_name is None:
+        experiment_name = datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    start_epoch = 0
 
-        tensorboard_writer = (
-            SummaryWriter(os.path.join("runs_tensorboard", str(experiment_name)))
-            if tensorboard_logging
-            else None
-        )
-        running_average_training_loss_logger = RunningAverageLogger()
-        val_loss_logger = RunningAverageLogger()
+    tensorboard_writer = (
+        SummaryWriter(os.path.join("runs_tensorboard", str(experiment_name)))
+        if tensorboard_logging
+        else None
+    )
+    running_average_training_loss_logger = RunningAverageLogger()
+    val_loss_logger = RunningAverageLogger()
 
-        printer = InplacePrinter(2 + len(metrics))
+    printer = InplacePrinter(2 + len(metrics))
 
-        for epoch in range(start_epoch, num_epoch):
-            with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
-                if printing:
-                    printer.reset()
-                    print("-" * 5 + f"EPOCH: {epoch}" + "-" * 5)
-                start = time.time()
+    for epoch in range(start_epoch, num_epoch):
+        with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+            if printing:
+                printer.reset()
+                print("-" * 5 + f"EPOCH: {epoch}" + "-" * 5)
+            start = time.time()
 
-                running_average_training_loss_logger.reset()
+            running_average_training_loss_logger.reset()
 
-                for metric in metrics.values():
-                    metric.reset()
+            for metric in metrics.values():
+                metric.reset()
 
-                for i, (x, y) in enumerate(train_set):
-                    y = (
-                        torch.tensor(y, dtype=torch.float32, requires_grad=True)
-                        .float()
-                        .to(predictor.model.device)
-                    )
-                    point_coords = extract_points_from_mask(y, num_points=num_points)
-                    point_labels = np.ones(point_coords.shape[0])
-                    predictor.set_image(x)
-                    # mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(point_coords=point_coords, point_labels=point_labels, normalize_coords=True, box=None, mask_logits=None)
-                    # sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels), boxes=None, masks=None)
+            for i, (x, y) in enumerate(train_set):
+                y = (torch.tensor(y) > 0).to(predictor.model.device).to(torch.float32)
+                point_coords = extract_points_from_mask(y, num_points=num_points)
+                point_labels = np.ones(point_coords.shape[0])
+                predictor.set_image(x)
+                # mask_input, unnorm_coords, labels, unnorm_box = predictor._prep_prompts(point_coords=point_coords, point_labels=point_labels, normalize_coords=True, box=None, mask_logits=None)
+                # sparse_embeddings, dense_embeddings = predictor.model.sam_prompt_encoder(points=(unnorm_coords, labels), boxes=None, masks=None)
 
-                    y_pred, _, _ = predictor.predict(
-                        point_coords=point_coords,
-                        point_labels=point_labels,
-                        return_logits=True,
-                    )
-                    y_pred = y_pred[0]
-                    mask = (y > 0).to(torch.long).cpu()
-                    mask_pred = (y_pred > predictor.mask_threshold).to(torch.long).cpu()
+                y_pred, _, _ = predictor.predict(
+                    point_coords=point_coords,
+                    point_labels=point_labels,
+                    return_logits=True,
+                )
+                y_pred = y_pred[0]
 
-                    loss = loss_fn(y_pred, y)
-                    loss.requires_grad_(True)
+                mask = (y > 0).to(torch.long).cpu()
+                mask_pred = (y_pred > predictor.mask_threshold).to(torch.long).cpu()
+                loss = loss_fn(y_pred, y)
 
-                    predictor.model.zero_grad()
-                    loss.backward()
-                    optimizer.step()
+                predictor.model.zero_grad()
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-                    epoch_metric_values = {}
-                    for metric_name, metric in metrics.items():
-                        epoch_metric_values[metric_name] = metric(
-                            mask_pred, mask
-                        ).item()
+                epoch_metric_values = {}
+                for metric_name, metric in metrics.items():
+                    epoch_metric_values[metric_name] = metric(mask_pred, mask).item()
 
-                    running_average_training_loss_logger.add_value(loss.item())
-
-                    log_metrics(
-                        None,
-                        global_step=epoch * len(train_set) + i,
-                        prefix="within_epoch_train/",
-                        extra={
-                            "epoch": epoch,
-                            "loss": loss.item(),
-                            "ratl": running_average_training_loss_logger.get_avg(),
-                            **epoch_metric_values,
-                        },
-                        log_wandb=False,
-                        tensorboard_writer=tensorboard_writer,
-                    )
-                    if i % 1 == 0 and i != 0:
-                        fraction_done = max(i / len(train_set), 1e-6)
-                        time_taken = time.time() - start
-                        if printing:
-                            for metric_name, metric in metrics.items():
-                                printer.print(
-                                    f"{metric_name} : {epoch_metric_values[metric_name]}"
-                                )
-                            printer.print(
-                                f"e: {epoch} | i: {i} | loss: {loss.item():2.3f} | ratl: {running_average_training_loss_logger.get_avg():2.3f}"
-                            )
-                            printer.print(
-                                f"{fraction_done*100:2.2f}% | est time left: {time_taken*(1-fraction_done)/fraction_done:.1f} s | est total: {time_taken/fraction_done:.1f} s"
-                            )
-
-                total_time_taken = time.time() - start
+                running_average_training_loss_logger.add_value(loss.item())
 
                 log_metrics(
-                    metrics,
-                    global_step=epoch,
-                    prefix="train/",
+                    None,
+                    global_step=epoch * len(train_set) + i,
+                    prefix="within_epoch_train/",
                     extra={
                         "epoch": epoch,
-                        "loss": running_average_training_loss_logger.get_avg(),
-                        "total_time": total_time_taken,
-                        "per_epoch_time": total_time_taken / len(train_set),
+                        "loss": loss.item(),
+                        "ratl": running_average_training_loss_logger.get_avg(),
+                        **epoch_metric_values,
                     },
-                    log_wandb=wandb_logging,
+                    log_wandb=False,
                     tensorboard_writer=tensorboard_writer,
                 )
-                if printing:
-                    print("--Train--")
-                    for metric_name, metric in metrics.items():
-                        print(f"{metric_name} : {metric.compute()}")
+                if i % 1 == 0 and i != 0:
+                    fraction_done = max(i / len(train_set), 1e-6)
+                    time_taken = time.time() - start
+                    if printing:
+                        for metric_name, metric in metrics.items():
+                            printer.print(
+                                f"{metric_name} : {epoch_metric_values[metric_name]}"
+                            )
+                        printer.print(
+                            f"e: {epoch} | i: {i} | loss: {loss.item():2.3f} | ratl: {running_average_training_loss_logger.get_avg():2.3f}"
+                        )
+                        printer.print(
+                            f"{fraction_done*100:2.2f}% | est time left: {time_taken*(1-fraction_done)/fraction_done:.1f} s | est total: {time_taken/fraction_done:.1f} s"
+                        )
+
+            total_time_taken = time.time() - start
+
+            log_metrics(
+                metrics,
+                global_step=epoch,
+                prefix="train/",
+                extra={
+                    "epoch": epoch,
+                    "loss": running_average_training_loss_logger.get_avg(),
+                    "total_time": total_time_taken,
+                    "per_epoch_time": total_time_taken / len(train_set),
+                },
+                log_wandb=wandb_logging,
+                tensorboard_writer=tensorboard_writer,
+            )
+            if printing:
+                print("--Train--")
                 for metric_name, metric in metrics.items():
-                    metric.reset()
+                    print(f"{metric_name} : {metric.compute()}")
+            for metric_name, metric in metrics.items():
+                metric.reset()
 
-                val_loss_logger.reset()
+            val_loss_logger.reset()
 
+            with torch.inference_mode():
                 for i, (x, y) in enumerate(val_set):
                     y = (
-                        torch.tensor(y, dtype=torch.float32, requires_grad=True)
-                        .float()
+                        (torch.tensor(y) > 0)
                         .to(predictor.model.device)
+                        .to(torch.float32)
                     )
                     point_coords = extract_points_from_mask(y, num_points=num_points)
                     point_labels = np.ones(point_coords.shape[0])
@@ -182,24 +177,33 @@ def train(
                     for metric_name, metric in metrics.items():
                         metric(mask_pred, mask)
 
-                if printing:
-                    print("--Val--")
-                    for metric_name, metric in metrics.items():
-                        print(f"{metric_name} : {metric.compute().item()}")
-                    print(
-                        f"train loss: {running_average_training_loss_logger.get_avg()} | val loss: {val_loss_logger.get_avg()}"
-                    )
-
-                log_metrics(
-                    metrics,
-                    global_step=epoch,
-                    prefix="val/",
-                    extra={
-                        "epoch": epoch,
-                        "loss": val_loss_logger.get_avg(),
-                    },
-                    log_wandb=wandb_logging,
-                    tensorboard_writer=tensorboard_writer,
+            if printing:
+                print("--Val--")
+                for metric_name, metric in metrics.items():
+                    print(f"{metric_name} : {metric.compute().item()}")
+                print(
+                    f"train loss: {running_average_training_loss_logger.get_avg()} | val loss: {val_loss_logger.get_avg()}"
                 )
+            if wandb_logging:
+                wandb.log(
+                    {
+                        "predict_vis": wandb.Image(
+                            generate_masks_with_points(
+                                predictor, val_set, num_points=num_points
+                            )
+                        )
+                    }
+                )
+            log_metrics(
+                metrics,
+                global_step=epoch,
+                prefix="val/",
+                extra={
+                    "epoch": epoch,
+                    "loss": val_loss_logger.get_avg(),
+                },
+                log_wandb=wandb_logging,
+                tensorboard_writer=tensorboard_writer,
+            )
 
-        return metrics
+    return metrics